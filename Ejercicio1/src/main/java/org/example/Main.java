6289accd05c8e025e593b39dd5c6a8ce343eb4f5
DAM <2061784@alu.murciaeduca.es>
2025-02-17 17:46:02 +0100

    primeros pasos

--- Ejercicio1/src/main/java/org/example/Main.java
+++ Ejercicio1/src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import java.io.File;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+
 public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Introduce el nombre del fichero que deseas cifrar:");
+        String nombrefichero= sc.nextLine();
+        File fichero=new File(nombrefichero);
+
+        while (!fichero.exists()) {
+            System.out.println("El fichero no existe, introduce uno valido");
+            nombrefichero= sc.nextLine();
+            fichero=new File(nombrefichero);
+        }
+
+        System.out.println("El fichero existe");
+
+        System.out.println("Ahora introduce la smeilla");
+
+
+        try{
+            SecureRandom secureRandom = SecureRandom.getInstance("AES");
+
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
