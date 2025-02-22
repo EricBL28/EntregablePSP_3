7d95df4248ad34f00ee538a120ae39d9d1c68853
EricBL28 <2061784@alu.murciaeduca.es>
2025-02-22 23:20:52 +0100

    HECHO

--- Ejercicio1/src/main/java/org/example/Main.java
+++ Ejercicio1/src/main/java/org/example/Main.java
@@ -1,39 +1,138 @@
 package org.example;
 
-import java.io.File;
+import javax.crypto.*;
+import java.io.*;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Scanner;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
+    private static final String ALGORITMO = "AES";
+    private static String nombrefichero = "";
+    private static final String ficheroEncriptado = "encriptado.zip";
 
-    public static void main(String[] args) {
+    private static SecretKey key;
 
+
+    public static void main(String[] args)  {
         System.out.println("Introduce el nombre del fichero que deseas cifrar:");
-        String nombrefichero= sc.nextLine();
-        File fichero=new File(nombrefichero);
+        nombrefichero = sc.nextLine();
+        File fichero = new File(nombrefichero);
 
         while (!fichero.exists()) {
-            System.out.println("El fichero no existe, introduce uno valido");
-            nombrefichero= sc.nextLine();
-            fichero=new File(nombrefichero);
+            System.out.println("El fichero no existe, introduce uno válido:");
+            nombrefichero = sc.nextLine();
+            fichero = new File(nombrefichero);
         }
 
-        System.out.println("El fichero existe");
+        System.out.println("El fichero existe. Introduce la semilla:");
+        String SEMILLA = sc.nextLine();
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.setSeed(SEMILLA.getBytes());//Se usa la semilla para generar la clave
 
-        System.out.println("Ahora introduce la smeilla");
+        key = generarLlave(secureRandom);
+        System.out.println("Clave generada:" + mostrarBytes(key.getEncoded())); //por curiosdad ver la clave que se genera
+
+        encriptarFichero();
+        descifrarFichero();
+    }
+
+    public static void encriptarFichero() {
+        System.out.println("Encriptando el fichero utilizando AES: " + ficheroEncriptado);
+
+        try (FileInputStream inputStream = new FileInputStream(nombrefichero);
+             FileOutputStream outputStream = new FileOutputStream(ficheroEncriptado)) {
+
+            int bytesLeidos;
+
+            Cipher cifrador = Cipher.getInstance(ALGORITMO);
+            //Se inicializa el cifrador en modo CIFRADO o ENCRIPTACIÓN
+            cifrador.init(Cipher.ENCRYPT_MODE, key);
+
+            byte[] buffer = new byte[1000]; //array de bytes
+            byte[] bufferCifrado;
+
+            //lee el fichero de 1k en 1k y pasa los fragmentos leidos al cifrador
+            bytesLeidos = inputStream.read(buffer, 0, 1000);
+            while (bytesLeidos != -1) {//mientras no se llegue al final del fichero
+                //pasa texto claro al cifrador y lo cifra, asignándolo a bufferCifrado
+                bufferCifrado = cifrador.update(buffer, 0, bytesLeidos);
+                outputStream.write(bufferCifrado); //Graba el texto cifrado en fichero
+                bytesLeidos = inputStream.read(buffer, 0, 1000);
+            }
+            bufferCifrado = cifrador.doFinal(); //Completa el cifrado
+            outputStream.write(bufferCifrado); //Graba el final del texto cifrado, si lo hay
 
 
-        try{
-            SecureRandom secureRandom = SecureRandom.getInstance("AES");
-
-
-        } catch (NoSuchAlgorithmException e) {
+        } catch (IOException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException |
+                 NoSuchAlgorithmException | InvalidKeyException e) {
             throw new RuntimeException(e);
         }
 
+        System.out.println("Archivo cifrado con éxito.");
 
+    }
+
+    public static void descifrarFichero() {
+        System.out.println("Encriptando el fichero utilizando AES: " + ficheroEncriptado);
+
+        String ficheroDesencriptado = "desencriptado.zip";
+        try (FileInputStream inputStream = new FileInputStream(ficheroEncriptado);
+             FileOutputStream outputStream = new FileOutputStream(ficheroDesencriptado)) {
+
+            int bytesLeidos;
+
+            Cipher cifrador = Cipher.getInstance(ALGORITMO);
+            //3.- Poner cifrador en modo DESCIFRADO o DESENCRIPTACIÓN
+            cifrador.init(Cipher.DECRYPT_MODE, key);
+            System.out.println("Desencpripto el fichero: " + ficheroEncriptado + ", utilizando AES : " + ficheroDesencriptado);
+
+            byte[] bufferClaro;
+            byte[] buffer = new byte[1000]; //array de bytes
+            //lee el fichero de 1k en 1k y pasa los fragmentos leidos al cifrador
+            bytesLeidos = inputStream.read(buffer, 0, 1000);
+            while (bytesLeidos != -1) {//mientras no se llegue al final del fichero
+                //pasa texto cifrado al cifrador y lo descifra, asignándolo a bufferClaro
+                bufferClaro = cifrador.update(buffer, 0, bytesLeidos);
+                outputStream.write(bufferClaro); //Graba el texto claro en fichero
+                bytesLeidos = inputStream.read(buffer, 0, 1000);
+            }
+            bufferClaro = cifrador.doFinal(); //Completa el descifrado
+            outputStream.write(bufferClaro); //Graba el final del texto claro, si lo hay
+
+
+        } catch (IOException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException |
+                 NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Archivo cifrado con éxito.");
+
+    }
+
+    public static SecretKey generarLlave(SecureRandom secureRandom) {
+        System.out.println("Genero la clave secreta con AES");
+        SecretKey clave = null;
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITMO);
+            keyGen.init(128, secureRandom);
+            clave = keyGen.generateKey();
+
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return clave;
+    }
+
+    public static String mostrarBytes(byte[] buffer) {
+        Base64.Encoder encoder = Base64.getEncoder();
+        return encoder.encodeToString(buffer);
+        //System.out.write(buffer, 0, buffer.length);
     }
 }
--- Ejercicio2/src/main/java/org/example/Main.java
+++ Ejercicio2/src/main/java/org/example/Main.java
@@ -1,9 +1,114 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.crypto.*;
+import java.io.*;
+import java.security.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-        System.out.println("Hello, World!");
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+    private static final String ALGORITMO = "RSA";
+    private static String nombrefichero = "";
+    private static final String ficheroEncriptado = "encriptado.zip";
+
+    private static final String USO ="RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
+
+    private static PrivateKey clavePrivada;
+    private static PublicKey clavePublica;
+
+
+    public static void main(String[] args)  {
+        System.out.println("Introduce el nombre del fichero que deseas cifrar:");
+        nombrefichero = sc.nextLine();
+        File fichero = new File(nombrefichero);
+
+        while (!fichero.exists()) {
+            System.out.println("El fichero no existe, introduce uno válido:");
+            nombrefichero = sc.nextLine();
+            fichero = new File(nombrefichero);
+        }
+
+        System.out.println("El fichero existe. Introduce la semilla:");
+        String SEMILLA = sc.nextLine();
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.setSeed(SEMILLA.getBytes());
+
+        generarLlave(secureRandom);
+
+        encriptarFichero();
+        descifrarFichero();
+        System.out.println("Cifrado asimetrico completado con éxito.");
+    }
+
+    public static void encriptarFichero() {
+        System.out.println("Encriptando el fichero utilizando " + USO + ": " + ficheroEncriptado);
+
+        try (FileInputStream inputStream = new FileInputStream(nombrefichero);
+             FileOutputStream outputStream = new FileOutputStream(ficheroEncriptado)) {
+
+            Cipher cifrador = Cipher.getInstance(USO);
+            cifrador.init(Cipher.ENCRYPT_MODE, clavePublica);
+
+            byte[] buffer = new byte[62]; // Tamaño máximo permitido
+            int bytesLeidos;
+
+            while ((bytesLeidos = inputStream.read(buffer)) != -1) {
+                byte[] bufferCifrado = cifrador.doFinal(buffer, 0, bytesLeidos);
+                outputStream.write(bufferCifrado);
+            }
+
+        } catch (IOException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException |
+                 NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Archivo cifrado con éxito.");
+    }
+
+    public static void descifrarFichero() {
+        System.out.println("Desencriptando el fichero utilizando " + USO + ": " + ficheroEncriptado);
+
+        String ficheroDesencriptado = "desencriptado.zip";
+        try (FileInputStream inputStream = new FileInputStream(ficheroEncriptado);
+             FileOutputStream outputStream = new FileOutputStream(ficheroDesencriptado)) {
+
+            Cipher cifrador = Cipher.getInstance(USO);
+            cifrador.init(Cipher.DECRYPT_MODE, clavePrivada);
+
+            byte[] buffer = new byte[128];
+            int bytesLeidos;
+
+            while ((bytesLeidos = inputStream.read(buffer)) != -1) {
+                byte[] bufferClaro = cifrador.doFinal(buffer, 0, bytesLeidos);
+                outputStream.write(bufferClaro);
+            }
+
+        } catch (IOException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException |
+                 NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Archivo descifrado con éxito.");
+    }
+
+
+    public static void generarLlave(SecureRandom secureRandom) {
+        System.out.println("Genero la claves privada y publica con RSA");
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITMO);
+            keyGen.initialize(1024,secureRandom);
+            KeyPair key = keyGen.generateKeyPair();
+
+            clavePrivada= key.getPrivate();
+            clavePublica= key.getPublic();
+
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
 
     }
+
 }
